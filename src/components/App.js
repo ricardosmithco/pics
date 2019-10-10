1a4c464a9e838b8b6f6d5d35f91e92e74d1f6d84
redelization <ricardosmithco@gmail.com>
2019-10-10 15:20:57 -0500

    refactored App.js to a class component; created a prop method to link App.js to SearchBar.js; implemented axios to make API call to unsplashed to search photos

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
+import { throwStatement } from '@babel/types';
 
-const App = () => {
-    return <div className="ui container" style={{marginTop: '10px'}}>
-        <SearchBar />
-    </div>
+
+class App extends React.Component {
+    onSearchSubmit(term){
+        axios.get('https://api.unsplash.com/search/photos/', {
+            params: {query: term},
+            headers: {Authorization: 'Client-ID 7f36325a9bce5467371cae58a4faaa7442d904ea4b84e0495b2339386b5b4e2a'}
+        }); 
+    }
+
+    render(){
+        return (
+        <div className="ui container" style={{marginTop: '10px'}}>
+            <SearchBar onSubmit={this.onSearchSubmit}/>
+        </div>
+        );
+    }
 }
 
 export default App;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,13 +3,19 @@ import React from 'react';
 class SearchBar extends React.Component{
     state = { term: '' };
 
+    onFormSubmit = (event) => {
+        event.preventDefault();
+
+        this.props.onSubmit(this.state.term);
+    }
+
     render(){
         return ( 
         <div className="ui segment">
-            <form className="ui form">
+            <form onSubmit={this.onFormSubmit} className="ui form">
                 <div className="field">
-                    <input type="text" value ={this.state.term} 
-                    onChange={(event) => this.setState({ term: event.target.vlaue })}/> 
+                    <label>Image Search</label>
+                    <input type="text" value ={this.state.term} onChange={e => this.setState({ term: e.target.value }) } /> 
                 </div>
             </form>
         </div>); // notice line 11: we did not put a set of parenthesis on this.onInputChange. 
